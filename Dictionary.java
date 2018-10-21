67fe680efcd7245148f9071540ba3c4d8e734bea
Ha Quang Hung <37433499+quanghung27@users.noreply.github.com>
2018-10-21 21:18:25 +0700

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dictionarys;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author MANH HOANG
- */
 public class Dictionary {
     ArrayList<Word> list = new ArrayList<>();
 }
