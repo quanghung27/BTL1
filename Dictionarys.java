98da9196faba20665457db661397462ce6ff8731
Ha Quang Hung <37433499+quanghung27@users.noreply.github.com>
2018-10-21 21:24:54 +0700

    Update Dictionarys.java

--- Dictionarys.java
+++ Dictionarys.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dictionarys;
 
 import java.util.Scanner;
 
-/**
- *
- * @author MANH HOANG
- */
 public class Dictionarys {
 
     public void menu() {
