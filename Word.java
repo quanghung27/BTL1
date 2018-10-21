eb5ee6ce958593b77663d282851c7d7aab9bd998
Ha Quang Hung <37433499+quanghung27@users.noreply.github.com>
2018-10-21 21:17:36 +0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dictionarys;
 
-/**
- *
- * @author MANH HOANG
- */
 public class Word {
     private String Word_target;
     private String Word_explain;
